4a16bc125ccb30e71b35c28e987898fdfe5d7517
Tristan Chan <126594566+TristanChanCraig@users.noreply.github.com>
2025-05-08 15:28:51 +0000

    5/8/25

--- main.py
+++ main.py
@@ -13,5 +13,59 @@ for table in tables:
 df = pd.read_sql_query('SELECT * FROM albums', conn)
 print(df)
 
-cur.execute('INSERT INTO albums (AlbumId, Title, ArtistId) VALUES (7777, hi, 77777)', conn)
+# cur.execute('INSERT INTO albums (AlbumId, Title, ArtistId) VALUES (7777, "oof", 77777)', conn)
+# print(df)
+
+cur.execute('INSERT INTO artists (ArtistID, Name) VALUES (276, "Braeden")')
+df = pd.read_sql_query('SELECT * FROM artists', conn)
 print(df)
+
+
+# cur.execute('SELECT * FROM playlists', conn)
+# print(df)
+# cur.execute('SELECT * FROM playlist_track', conn)
+# print(df)
+# cur.execute('SELECT * FROM tracks', conn)
+# print(df) 
+cur.execute('''SELECT Title, Name
+FROM albums  
+INNER JOIN artists 
+ON albums.ArtistId = artists.ArtistId''')
+result = cur.fetchall() 
+for row in result: 
+    print(row) 
+
+df = pd.read_sql_query('SELECT * FROM invoices', conn)
+print(df)
+
+avgTotal = df['Total'].mean()
+print("The average of all invoices costed: ${:.3}".format(avgTotal))
+
+modeBillingCountry = df['BillingCountry'].mode()
+print("The most common buyers are from: {}".format(modeBillingCountry))
+
+df = pd.read_sql_query('SELECT * FROM invoice_items', conn)
+print(df)
+# df = pd.read_sql_query('SELECT * FROM tracks', conn)
+# print(df)
+
+cur.execute('''SELECT Total, UnitPrice
+FROM invoices  
+LEFT JOIN invoice_items 
+USING ('InvoiceID')''')
+# result = cur.fetchall() 
+# for row in result: 
+#     print(row) 
+# albums
+# sqlite_sequence
+# artists
+# customers
+# employees
+# genres
+# invoices
+# invoice_items
+# media_types
+# playlists
+# playlist_track
+# tracks
+# sqlite_stat1

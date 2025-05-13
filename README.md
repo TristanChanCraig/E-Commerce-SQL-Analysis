87e8e087b911ce837d32babccb4cb368ae6dfe53
Tristan Chan <126594566+TristanChanCraig@users.noreply.github.com>
2025-05-13 15:26:56 +0000

    5/13/25

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # E-Commerce-SQL-Analysis
+
+### The average of all invoices costed: $5.65
+Most albums were cheap and costed less than $10.
+
+### The total amount of invoices costed: $412
+People bought albums that costed a total of $412, which is around 800 as the average invoice was around $5.
+
+### The largest invoice costed: $25.86
+The highest costing album was $25.86.
+
+### The most common buyers are from: USA
+The most frequent buyers were from the US, indicating popularity of albums there.
+
+### There were 24 unique countries billed
+There were many European and American countries billed that frequently bought albums.
+
+### The most frequent customer is: 2 (Leonie Köhler)
+Leonie buys the most albums out of anyone.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas as pd
 import database_connector as dc
+from statistics import mode
 
 conn = dc.main()
 cur = conn.cursor()
@@ -11,14 +12,14 @@ for table in tables:
     print(table[0])
 
 df = pd.read_sql_query('SELECT * FROM albums', conn)
-print(df)
+# print(df)
 
 # cur.execute('INSERT INTO albums (AlbumId, Title, ArtistId) VALUES (7777, "oof", 77777)', conn)
 # print(df)
 
 cur.execute('INSERT INTO artists (ArtistID, Name) VALUES (276, "Braeden")')
 df = pd.read_sql_query('SELECT * FROM artists', conn)
-print(df)
+# print(df)
 
 
 # cur.execute('SELECT * FROM playlists', conn)
@@ -32,27 +33,37 @@ FROM albums
 INNER JOIN artists 
 ON albums.ArtistId = artists.ArtistId''')
 result = cur.fetchall() 
-for row in result: 
-    print(row) 
+# print(mode(result[1](0)), "=========================================================================")
+# print(type(result), "===================================================================")
+# for row in result: 
+#    print(row) 
 
 df = pd.read_sql_query('SELECT * FROM invoices', conn)
-print(df)
-
-avgTotal = df['Total'].mean()
-print("The average of all invoices costed: ${:.3}".format(avgTotal))
-
-modeBillingCountry = df['BillingCountry'].mode()
-print("The most common buyers are from: {}".format(modeBillingCountry))
-
-df = pd.read_sql_query('SELECT * FROM invoice_items', conn)
-print(df)
-# df = pd.read_sql_query('SELECT * FROM tracks', conn)
 # print(df)
 
-cur.execute('''SELECT Total, UnitPrice
-FROM invoices  
-LEFT JOIN invoice_items 
-USING ('InvoiceID')''')
+modeCustomerId = mode(df['CustomerId'])
+print("The most frequent customer is: ", modeCustomerId)
+
+avgTotal = df['Total'].describe()
+print("The description of total invoices: ${}".format(avgTotal))
+
+modeBillingCountry = df['BillingCountry'].describe()
+print("The description of countries billed: {}".format(modeBillingCountry))
+
+df = pd.read_sql_query('SELECT * FROM invoice_items', conn)
+# print(df)
+
+modeTrackId = df['TrackId'].mode()
+# df = pd.read_sql_query('SELECT * FROM customers', conn)
+# print(df)
+
+
+# df = pd.read_sql_query('SELECT * FROM tracks', conn)
+# print(df)
+# cur.execute('''SELECT Total, UnitPrice
+# FROM invoices  
+# LEFT JOIN invoice_items 
+# USING ('InvoiceID')''')
 # result = cur.fetchall() 
 # for row in result: 
 #     print(row) 
